78b14c26b57fd636c842ca0b826fe1b31da2b307
Jenn Purnell <jennpurnell@gmail.com>
2017-01-30 11:56:06 -0800

    Integrated existing Cozy Coworking program functions (for Host class) into switch case 2, added console prompts so the user can enter values for all existing variables.

--- Cozy_Coworking/CoCoProgram.cs
+++ Cozy_Coworking/CoCoProgram.cs
@@ -11,115 +11,166 @@ namespace Cozy_Coworking
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("*****Welcome to Cozy Coworking!*****");
+            Console.WriteLine("*****Welcome to Cozy Coworking!*****");
+      
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Become a Coworker");
+            Console.WriteLine("2. Become a Host");
+            Console.WriteLine("3. Already have an Account? Login:");
+            Console.WriteLine("Please select one of the options from above:");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "0":
+                    Console.WriteLine("This option is under construction - please try again later:");
+                    return;
+                case "1":
+                    Console.WriteLine("This option is under construction - please try again later:");
+                    return;
+                case "2":
+                    Console.WriteLine("Please enter your user name:");
+                    var userName = Console.ReadLine();
+                    Console.WriteLine("Please enter your email address:");
+                    var emailAddress = Console.ReadLine();
+                    var myHost = new Host(userName, emailAddress);
 
-            //Console.WriteLine("0. Exit");
-            //Console.WriteLine("1. Become a Coworker");
-            //Console.WriteLine("2. Become a Host");
-            //Console.WriteLine("3. Already have an Account? Login:");
-            //Console.WriteLine("Please select one of the options from above:");
-            //var choice = Console.ReadLine();
-            //switch (choice)
+                    Console.WriteLine("Please enter the number of coworking spaces you have available:");
+                    var myAvailability = Convert.ToInt32(Console.ReadLine());
+                    myHost.AddAvailability(myAvailability);
+                    Console.Write($"{myHost.UserName} has {myHost.Availability} coworking spots available");
+
+                    myHost.DogCatFriendly = true;
+
+                    myHost.Description = ($" in a beautiful, quiet house in Redmond with desks for your coworking needs. Location {myHost.LocationID}");
+                    Console.WriteLine(myHost.Description);
+
+                    #region Rating
+                    //var myRating = myHost.CreateRating(int myStars);
+                    Console.WriteLine($"Rate {myHost.UserName} as a host. Please enter a number between 1 and 5.");
+                    int myStars = Convert.ToInt32(Console.ReadLine());
+                    //var myRating = myHost.CreateRating(myStars)
+                    //Console.WriteLine($"{myHost.UserName} has a rating of {myHost.Rating}");
+                    {
+                        if (myStars == 1)
+                        {
+                            Console.WriteLine($"{myHost.UserName} has a rating of *");
+                        }
+                        if (myStars == 2)
+                        {
+                            Console.WriteLine($"{myHost.UserName} has a rating of **");
+                        }
+                        if (myStars == 3)
+                        {
+                            Console.WriteLine($"{myHost.UserName} has a rating of ***");
+                        }
+                        if (myStars == 4)
+                        {
+                            Console.WriteLine($"{myHost.UserName} has a rating of ****");
+                        }
+                        if (myStars == 5)
+                        {
+                            Console.WriteLine($"{myHost.UserName} has a rating of *****");
+                        }
+                        //else
+                        //    Console.WriteLine($"Not a valid rating");
+                    }
+                    #endregion
+                    return;
+
+                case "3":
+                    Console.WriteLine("This option is under construction - please try again later:");
+                    return;
+            }
+
+
+
+            ////create an account object/instance
+            //var myHost = new Host();
+            //myHost.UserName = "Alice";
+            //myHost.EmailAddress = "test@test.com";
+
+            //var myAvailability = myHost.AddAvailability(4);
+            //Console.Write ($"{myHost.UserName} has {myHost.Availability} coworking spots available");
+
+            //myHost.DogCatFriendly = true;
+
+            //myHost.Description = ($" in a beautiful, quiet house in Redmond with desks for your coworking needs. Location {myHost.LocationID}");
+            //Console.WriteLine(myHost.Description);
+
+            //#region Rating
+            ////var myRating = myHost.CreateRating(int myStars);
+            //Console.WriteLine($"Rate {myHost.UserName} as a host. Please enter 1-5 stars.");
+            //int myStars = Convert.ToInt32(Console.ReadLine());
+            ////var myRating = myHost.CreateRating(myStars)
+            ////Console.WriteLine($"{myHost.UserName} has a rating of {myHost.Rating}");
             //{
-            //    case "0":
-            //        return;
-            //    case "1":
-            //        var myCoworker = new Coworker();
-            //        var emailAddress = Console.ReadLine();
-            //        var myAccount = new UserName(emailAddress);
-            //    case "2":
-
+            //    if (myStars == 1)
+            //    {
+            //        Console.WriteLine($"{myHost.UserName} has a rating of *");
+            //    }
+            //    if (myStars == 2)
+            //    {
+            //        Console.WriteLine($"{myHost.UserName} has a rating of **");
+            //    }
+            //    if (myStars == 3)
+            //    {
+            //        Console.WriteLine($"{myHost.UserName} has a rating of ***");
+            //    }
+            //    if (myStars == 4)
+            //    {
+            //        Console.WriteLine($"{myHost.UserName} has a rating of ****");
+            //    }
+            //    if (myStars == 5)
+            //    {
+            //        Console.WriteLine($"{myHost.UserName} has a rating of *****");
+            //    }
+            //    //else
+            //    //    Console.WriteLine($"Not a valid rating");
             //}
+            //#endregion
 
-            
+            //var myHost2 = new Host("Rohit", "test@test.com");
 
-            //create an account object/instance
-            var myHost = new Host();
-            myHost.UserName = "Alice";
-            myHost.EmailAddress = "test@test.com";
+            //myAvailability = myHost2.AddAvailability(3);
+            //Console.Write($"{myHost2.UserName} has {myHost2.Availability} coworking spots available");
 
-            var myAvailability = myHost.AddAvailability(4);
-            Console.Write ($"{myHost.UserName} has {myHost.Availability} coworking spots available");
+            //myHost2.DogCatFriendly = false;
 
-            myHost.DogCatFriendly = true;
+            //myHost2.Description = ($" in a beautiful, quiet house in Redmond with desks for your coworking needs. Location {myHost2.LocationID}");
+            //Console.WriteLine(myHost2.Description);
 
-            myHost.Description = ($" in a beautiful, quiet house in Redmond with desks for your coworking needs. Location {myHost.LocationID}");
-            Console.WriteLine(myHost.Description);
+            //#region Rating
+            ////var myRating = myHost.CreateRating(int myStars);
+            //Console.WriteLine($"Rate {myHost2.UserName} as a host. Please enter 1-5 stars.");
+            //myStars = Convert.ToInt32(Console.ReadLine());
 
-            #region Rating
-            //var myRating = myHost.CreateRating(int myStars);
-            Console.WriteLine($"Rate {myHost.UserName} as a host. Please enter 1-5 stars.");
-            int myStars = Convert.ToInt32(Console.ReadLine());
-            //var myRating = myHost.CreateRating(myStars)
-            //Console.WriteLine($"{myHost.UserName} has a rating of {myHost.Rating}");
-            {
-                if (myStars == 1)
-                {
-                    Console.WriteLine($"{myHost.UserName} has a rating of *");
-                }
-                if (myStars == 2)
-                {
-                    Console.WriteLine($"{myHost.UserName} has a rating of **");
-                }
-                if (myStars == 3)
-                {
-                    Console.WriteLine($"{myHost.UserName} has a rating of ***");
-                }
-                if (myStars == 4)
-                {
-                    Console.WriteLine($"{myHost.UserName} has a rating of ****");
-                }
-                if (myStars == 5)
-                {
-                    Console.WriteLine($"{myHost.UserName} has a rating of *****");
-                }
-                //else
-                //    Console.WriteLine($"Not a valid rating");
-            }
-            #endregion
-
-            var myHost2 = new Host("Rohit", "test@test.com");
-
-            myAvailability = myHost2.AddAvailability(3);
-            Console.Write($"{myHost2.UserName} has {myHost2.Availability} coworking spots available");
-
-            myHost2.DogCatFriendly = false;
-
-            myHost2.Description = ($" in a beautiful, quiet house in Redmond with desks for your coworking needs. Location {myHost2.LocationID}");
-            Console.WriteLine(myHost2.Description);
-
-            #region Rating
-            //var myRating = myHost.CreateRating(int myStars);
-            Console.WriteLine($"Rate {myHost2.UserName} as a host. Please enter 1-5 stars.");
-            myStars = Convert.ToInt32(Console.ReadLine());
-
-            {
-                if (myStars == 1)
-                {
-                    Console.WriteLine($"{myHost2.UserName} has a rating of *");
-                }
-                if (myStars == 2)
+            //{
+            //    if (myStars == 1)
+            //    {
+            //        Console.WriteLine($"{myHost2.UserName} has a rating of *");
+            //    }
+            //    if (myStars == 2)
 
 
-                {
-                    Console.WriteLine($"{myHost2.UserName} has a rating of **");
-                }
-                if (myStars == 3)
-                {
-                    Console.WriteLine($"{myHost2.UserName} has a rating of ***");
-                }
-                if (myStars == 4)
-                {
-                    Console.WriteLine($"{myHost2.UserName} has a rating of ****");
-                }
-                if (myStars == 5)
-                {
-                    Console.WriteLine($"{myHost2.UserName} has a rating of *****");
-                }
-                //else
-                //    Console.WriteLine($"Not a valid rating");
-            }
-            #endregion
+            //    {
+            //        Console.WriteLine($"{myHost2.UserName} has a rating of **");
+            //    }
+            //    if (myStars == 3)
+            //    {
+            //        Console.WriteLine($"{myHost2.UserName} has a rating of ***");
+            //    }
+            //    if (myStars == 4)
+            //    {
+            //        Console.WriteLine($"{myHost2.UserName} has a rating of ****");
+            //    }
+            //    if (myStars == 5)
+            //    {
+            //        Console.WriteLine($"{myHost2.UserName} has a rating of *****");
+            //    }
+            //    //else
+            //    //    Console.WriteLine($"Not a valid rating");
+            //}
+            //#endregion
 
         }
     }
